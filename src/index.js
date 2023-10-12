a5ed2ce451d67c2797485f9899a4e0e17cde16dc
CodeLHC <lhcrawford97@gmail.com>
2023-10-12 13:53:48 +0100

    inital project class setup

--- src/index.js
+++ src/index.js
@@ -2,3 +2,18 @@ import { Item } from "./itemClass";
 import { Project } from "./projectClass";
 
 console.log("hi Crawfs");
+
+const testItem = new Item(
+  "Test this",
+  "test that its working desc",
+  "5th OCT",
+  "high"
+);
+console.log(testItem);
+
+const testProject = new Project("testProject Name");
+
+console.log(testProject);
+testProject.addItem(testItem);
+
+console.log(testProject.list);
--- src/projectClass.js
+++ src/projectClass.js
@@ -1,10 +1,24 @@
 class Project {
-  constructor() {}
+  constructor(name) {
+    this.name = name;
+    this.list = [];
+  }
 
   //functions
+
+  addItem(item) {
+    this.list.push(item);
+  }
+
+  //   removeItem(itemToRemove) {
+  //     const findItemIndex = this.list.findIndex(itemToRemove);
+  //     const updatedShipArray = this.list.splice(findItemIndex, 1);
+  //   }
 }
 
 export { Project };
 
 // 	- Name
 // Empty project array to push items to?
+
+// iife function to add project to list of projects to choose from on initialization?
